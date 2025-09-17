e002ecb23ae9ce64abef6e30bb5b55ed43f75ff8
Travis Lockcuff <tlockcuff@gmail.com>
2025-09-17 07:35:54 -0400

    Updating commands

--- backup.sh
+++ backup.sh
@@ -2,4 +2,4 @@
 
 # backup the convex database
 mkdir -p backups
-npx --yes convex backup --include-file-storage --path ./backups/backup.zip
+npx --yes convex export --include-file-storage --path ./backups/backup.zip
